0d8aa410593b4264ec83e4172d4f983097e14b40
hellomynameis-cell <195034782+hellomynameis-cell@users.noreply.github.com>
2025-01-29 21:33:09 +0100

    s

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
+
+
+
+
+
 let humanScore = 0;
 let computerScore = 0;
+
+const rock = document.querySelector("#Rock");
+rock.addEventListener("click", () => {
+    humanChoice = "Rock";
+    playGame();
+  });
+
+const scissors = document.querySelector("#Scissors");
+scissors.addEventListener("click", () => {
+    humanChoice = "Scissors";
+    playGame();
+  });
+
+const paper = document.querySelector("#Paper");
+paper.addEventListener("click", () => {
+    humanChoice = "Paper";
+    playGame();
+  });
+
+
+const result =document.querySelector("#result");
+const score =document.querySelector("#score");
+
+
+
+//btn.addEventListener("click", playGame());
+
 
 function getComputerChoice() {
     let computerMove = "";
@@ -19,37 +51,44 @@ function getComputerChoice() {
     return computerMove
 }
 
-function getHumanChoice() {
-    humanMove = prompt("Rock, Paper, or Scissors? ").toLowerCase();
+//function getHumanChoice() {
+  //  humanMove = prompt("Rock, Paper, or Scissors? ").toLowerCase();
 
-    return humanMove.charAt(0).toUpperCase() + humanMove.slice(1)
-}
+   // return humanMove.charAt(0).toUpperCase() + humanMove.slice(1)
+//}
 
 function playGame() {
     function playRound(humanChoice, computerChoice) {
         if (humanChoice == computerChoice) {
-            console.log("It's a tie")
+            result.textContent = "Tie!"
         } else if(
             humanChoice == "Rock" && computerChoice == "Scissors" ||
             humanChoice == "Paper" && computerChoice == "Rock" ||
             humanChoice == "Scissors" && computerChoice == "Paper"
         ) {
             humanScore += 1;
-            console.log(`You win!! ${humanChoice} beats ${computerChoice}`);
+            result.textContent = "You win";
         } else {
             computerScore += 1;
-            console.log(`You lose!! ${computerChoice} beats ${humanChoice}`);
+            result.textContent = "You lose";
         }
     }
-    console.log("This is the result of your RPS game")
-    for (let i = 0; i < 5; i++) {
-        let humanChoice = getHumanChoice();
-        let computerChoice = getComputerChoice();
-        console.log(`Round ${i + 1}`)
-        playRound(humanChoice, computerChoice);
-        console.log(`Current Score => Computer: ${computerScore} vs Human: ${humanScore}`)
-    }
+    //console.log("This is the result of your RPS game")
+    
+    
+    
+    //for (let i = 0; i < 5; i++) {
+    //let humanChoice = getHumanChoice();
+    let computerChoice = getComputerChoice();
+    //console.log(`Round ${i + 1}`)
+    playRound(humanChoice, computerChoice);
+    let text = "Computer "+computerScore +" vs Human:" + humanScore;
+    console.log (text);
+    score.textContent = text;
+
+    if (computerScore>4) alert ("You lose!");
+    if (humanScore>4) alert ("You win!");
+    //}
 
 }
 
-playGame();
